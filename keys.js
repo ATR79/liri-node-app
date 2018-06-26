d93fa0f8f61035de6b1f348ebf8a4410e2e5cb3e
Anna Rutherford <anna.t.rutherford@gmail.com>
2018-06-25 20:16:38 -0400

    API Keys Added

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+    TWITTER_CONSUMER_KEY: "SZzsigw3iqE0pSADBUJkENCab",
+    TWITTER_CONSUMER_SECRET: "AMw4tYFP7an0lIul49C21aS9fk2cWZTGS8XVE9bjb2JFOlNa36",
+    TWITTER_ACCESS_TOKEN_KEY: "2722037155-UARaU6pdJvvjerc1jKNuZXxprSnk4bPlbIHqY81",
+    TWITTER_ACCESS_TOKEN_SECRET: "wnpv3qmcRgT4tBLvzFEnPAqU9ih2fKkLtbE95ZIFXAaQo"
+};
+
+exports.spotify = {
+    id: "df767b5960594c87868002228a76f9ee",
+    secret: "1de07cf55de74ef98d0bfc1719b574b0"
+};

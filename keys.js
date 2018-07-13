4ad29eb1c46a8e7f51df47368e29b15fcf833dd0
Anna Rutherford <anna.t.rutherford@gmail.com>
2018-07-13 16:25:48 -0400

    keys updated

--- keys.js
+++ keys.js
@@ -1,14 +1,14 @@
 console.log('this is loaded');
 
 exports.twitter = {
-    TWITTER_CONSUMER_KEY: "UMoZ39euhXt7GopAfmEfcoqrq",
-    TWITTER_CONSUMER_SECRET: "Tb9Sp3reyhZ8i5AE7w8YjhPPSk7SeRnmmTwXKrNpP8OzrQCVyQ",
-    TWITTER_ACCESS_TOKEN_KEY: "1011288800289591296-CH19qyK7i5pzMmHecvvsH56CiLPd8P",
-    TWITTER_ACCESS_TOKEN_SECRET: "97Bxus8jmwZ8g91Q17jzk2NljJg8YYhE5K5LLI2tONcas"
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-    id: "df767b5960594c87868002228a76f9ee",
-    secret: "1de07cf55de74ef98d0bfc1719b574b0"
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
 };
 
